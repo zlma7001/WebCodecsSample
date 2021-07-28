eed2833814e602a928e1650ef32952a62b820305
Zhaoliang <zlma7001@gmail.com>
2021-07-28 22:00:45 +0800

    webcodecs js samples: updated with latest api and use L1T3 encoding
    As mentioned, this CL updated the depreated VTR to MSTP, also update
    VideoFrame.createImageBitmap to createImageBitmap(VideoFrame). this
    CL also use L1T3 temporal layer encoding for testing purpose.

---
+++ webcodecs/main.js
@@ -0,0 +1,226 @@
+/*
+ *  Copyright (c) 2021.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+
+startButton.disabled = false;
+stopButton.disabled = true;
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+
+const userMedia = document.getElementById('userMedia');
+const playbackCanvas = document.getElementById("playbackCanvas");
+
+function getEncoderVideoCodec() {
+  const videoCodecSelect = document.querySelector('#videoCodec');
+  const option = videoCodecSelect.options[videoCodecSelect.selectedIndex];
+  return option.value;
+}
+
+function getEncoderFramerate() {
+  const videoFrameRateSelect = document.querySelector('#videoFrameRate');
+  const option = videoFrameRateSelect.options[videoFrameRateSelect.selectedIndex];
+  return option.value;
+}
+
+function getSelectedVideoBitrate() {
+  const bitrate = document.querySelector('#videoBitrate');
+  return bitrate.value;
+}
+
+function getSelectedVideoResolution() {
+  const videoFrameRateSelect = document.querySelector('#videoResolution');
+  const option = videoFrameRateSelect.options[videoFrameRateSelect.selectedIndex];
+  return option.value;
+}
+
+function getVideoEncoderAcceleration() {
+  const videoEncoderAccelerationSelect = document.querySelector('#acceleration');
+  const option = videoEncoderAccelerationSelect.options[videoEncoderAccelerationSelect.selectedIndex];
+  return option.value;
+}
+
+function getFrameSource() {
+  const frameSourceSelect = document.querySelector('#frameSource');
+  const option = frameSourceSelect.options[frameSourceSelect.selectedIndex];
+  return option.value;
+}
+
+function getUserMediaConstraints() {
+  let resolution = getSelectedVideoResolution().split('x');
+  let width = resolution[0];
+  let height = resolution[1];
+
+  let frameRate = getEncoderFramerate();
+
+  const constraints = {};
+  constraints.audio = false;
+  constraints.video = {};
+
+  constraints.video.width = {};
+  constraints.video.width.min = width;
+
+  constraints.video.width = constraints.video.width || {};
+  constraints.video.width.max = width;
+
+  constraints.video.height = {};
+  constraints.video.height.min = height;
+
+  constraints.video.height = constraints.video.height || {};
+  constraints.video.height.max = height;
+
+  constraints.video.frameRate = {};
+  constraints.video.frameRate.min = frameRate;
+
+  constraints.video.frameRate = constraints.video.frameRate || {};
+  constraints.video.frameRate.max = frameRate;
+
+  return constraints;
+}
+
+async function startUserMedia() {
+  if (userMedia.srcObject) {
+    userMedia.srcObject.getTracks().forEach(track => track.stop());
+    userMedia.srcObject = undefined;
+  }
+
+  const stream = await navigator.mediaDevices.getUserMedia(getUserMediaConstraints());
+  userMedia.srcObject = stream;
+
+  let mediaStreamTracks = stream.getVideoTracks();
+  console.log(mediaStreamTracks);
+
+  return (mediaStreamTracks[0]);
+}
+
+// webcodecs
+let stopped;
+
+let videoEncoder;
+let videoDecoder;
+
+let timer;
+
+function decoded_frame_available(video_frame) {
+  console.log(video_frame);
+  // TODO(zlma): can we draw video frame to <video> directly, maybe use MSTG?
+  drawFrameToCanvas(playbackCanvas, video_frame);
+}
+
+function decoder_error_callback(e) {
+  console.log(e.message);
+}
+
+function encoded_frame_availale(encoded_chunk, metadata) {
+  console.log(encoded_chunk);
+  console.log('encoded chunk metadata temporal layer index ', metadata.temporalLayerId);
+
+  if (videoDecoder == undefined) {
+    videoDecoder = createVideoDecoder(getEncoderVideoCodec(), decoded_frame_available, decoder_error_callback);
+  }
+
+  if (videoDecoder.decodeQueueSize >= 15) {
+    console.log('Stopped, too many queued frames ', videoDecoder.decodeQueueSize);
+    stopped = true;
+    return;
+  }
+
+  videoDecoder.decode(encoded_chunk);
+  console.log(videoDecoder);
+}
+
+function encoder_error_callback(e) {
+  console.log(e.message);
+}
+
+async function start() {
+  try {
+    startUserMedia()
+      .then((video_track) => {
+        const resolution = getSelectedVideoResolution().split('x');
+        const width = resolution[0];
+        const height = resolution[1];
+
+        const codec = getEncoderVideoCodec();
+        const acceleration = getVideoEncoderAcceleration();
+        const bitrate = getSelectedVideoBitrate();
+        const framerate = getEncoderFramerate();
+
+        videoEncoder = createVideoEncoder(codec, width, height, bitrate, framerate, acceleration,
+          encoded_frame_availale, encoder_error_callback);
+
+        let encodedFrames = 0;
+        let onVideoFrameAvailable = function (video_frame) {
+          console.log(video_frame);
+
+          videoEncoder.encode(video_frame, { keyFrame: (encodedFrames++ % 30 == 0) });
+          video_frame.close();
+        };
+
+        let frameSource = getFrameSource();
+        console.log(`frame source is ${frameSource}`);
+
+        if (frameSource == 'camera') {
+          const processor = new MediaStreamTrackProcessor({track: video_track});
+          const reader = processor.readable.getReader();
+          reader.read().then(function processFrame({done, value}) {
+            if (done) return;
+            onVideoFrameAvailable(value);
+            reader.read().then(processFrame);
+          });
+        } else { // camera->canvas->encoder...
+            timer = setInterval(async function () {
+              // Make sure the user media has video before we create |video_frame| from the user media.
+              if (userMedia.videoWidth > 0) {
+                let video_frame = await createVideoFrameFromUserMedia(userMedia, 1000 * Date.now());
+                onVideoFrameAvailable(video_frame);
+              }
+          }, 1000.0 / framerate);
+        }
+      });
+
+    stopped = false;
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  } catch (e) {
+    alert('start error!');
+  }
+}
+
+async function stop() {
+  console.log("stop");
+  stopped = true;
+
+  // stop getUserMedia
+  userMedia.srcObject.getVideoTracks()[0].stop();
+  userMedia.srcObject = null;
+
+  if (timer)
+    clearInterval(timer);
+  
+  await videoEncoder.flush();
+  videoEncoder.reset();
+  videoEncoder = undefined;
+
+  await videoDecoder.flush();
+  videoDecoder.reset();
+  videoDecoder = undefined;
+
+  // clear the video data on canvas when we exit
+  playbackCanvas.height = playbackCanvas.height;
+
+  playbackCanvas.srcObject = null;
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
---
+++ webcodecs/webcodecsUtils.js
@@ -0,0 +1,77 @@
+
+/*
+ *  Copyright (c) 2021.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+function createVideoDecoder(codec, output_cb, err_cb) {
+  try {
+    const video_decoder_init = {
+      output: output_cb,
+      error: err_cb,
+    };
+
+    let video_decoder = new VideoDecoder(video_decoder_init);
+
+    const params = {
+      codec: codec,
+    };
+    console.log(params);
+
+    video_decoder.configure(params);
+    return video_decoder;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function createVideoEncoder(codec, width, height, bitrate, framerate, acceleration, output_cb, err_cb) {
+  const video_encoder_init = {
+    output: output_cb,
+    error: err_cb,
+  };
+
+  let video_encoder = new VideoEncoder(video_encoder_init);
+
+  const params = {
+    codec: codec,
+    profile: codec,
+    width: width,
+    height: height,
+    bitrate: bitrate,
+    framerate: framerate,
+    acceleration: acceleration,
+    scalabilityMode: "L1T3",
+  };
+  console.log(params);
+
+  video_encoder.configure(params);
+  console.log(video_encoder);
+  return video_encoder;
+}
+
+async function drawFrameToCanvas(canvas, video_frame) {
+  canvas.width = video_frame.displayWidth;
+  canvas.height = video_frame.displayHeight;
+  let context = canvas.getContext("2d");
+
+  createImageBitmap(video_frame).then((toImageBitmap) => {
+    context.drawImage(toImageBitmap, 0, 0);
+  });
+}
+
+async function createVideoFrameFromUserMedia(usermedia, timestamp) {
+  let imageBitmap = await createImageBitmap(usermedia);
+
+  let video_frame_init = {
+    timestamp: timestamp,
+    duration: undefined,
+  }
+
+  return new VideoFrame(imageBitmap, video_frame_init);
+}
